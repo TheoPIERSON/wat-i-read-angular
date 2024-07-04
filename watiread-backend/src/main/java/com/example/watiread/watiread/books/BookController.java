aa13b1f9042541e2631171dcb27a09c6dab61c68
Theo Pierson <thheopierson@gmail.com>
2024-07-04 22:18:35 +0200

    feat: implement crud methods

--- watiread-backend/src/main/java/com/example/watiread/watiread/books/BookController.java
+++ watiread-backend/src/main/java/com/example/watiread/watiread/books/BookController.java
@@ -1,4 +1,39 @@
 package com.example.watiread.watiread.books;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Book>> getAllBooks (){
+        List<Book> books = (List<Book>) bookService.getAllBooks();
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+        Book newBook = bookService.addBook(book);
+        return new ResponseEntity<>(newBook, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Book> updateBook(@RequestBody Book book){
+        Book updateCustomer = bookService.updateBook(book);
+        return new ResponseEntity<>(updateCustomer, HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable("id") Long book_id){
+        bookService.deleteBook(book_id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
+
--- watiread-backend/src/main/java/com/example/watiread/watiread/books/BookService.java
+++ watiread-backend/src/main/java/com/example/watiread/watiread/books/BookService.java
@@ -1,4 +1,30 @@
 package com.example.watiread.watiread.books;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
 public class BookService {
+    private final BookRepository bookRepository;
+
+    public Iterable<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Transactional
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+    @Transactional
+    public Book updateBook(Book book){
+        return bookRepository.save(book);
+    }
+    @Transactional
+    public void deleteBook(Long id){
+        bookRepository.deleteById(id);
+    }
 }
